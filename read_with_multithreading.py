d27d6934daec64ca9a45f34ca1e8a690a6fa4607
Sherzod <sfreedom9393@gmail.com>
2024-08-07 10:13:15 +0500

    number of workers set into multithread method

--- read_with_multithreading.py
+++ read_with_multithreading.py
@@ -2,12 +2,16 @@ import os
 import cv2
 import concurrent.futures
 
+
+max_workers = 4  # number of threads
+
+
 def read_image(filename):
     return cv2.imread(filename)
 
 def read_images_multithreaded(directory):
     images = []
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = [executor.submit(read_image, os.path.join(directory, filename)) for filename in os.listdir(directory) if filename.endswith(".jpg") or filename.endswith(".png")]
         for future in concurrent.futures.as_completed(futures):
             img = future.result()
