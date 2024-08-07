48351af8ce712fc350a9bff8d8d58ad0653ff5a1
Sherzod <sfreedom9393@gmail.com>
2024-08-07 11:06:08 +0500

    added batch into tensorflow

--- read_with_tensorflow.py
+++ read_with_tensorflow.py
@@ -5,11 +5,15 @@ def decode_image(image):
     image = tf.image.resize(image, [224, 224])
     return image
 
-def read_images_tensorflow(directory):
+
+def read_images_tensorflow(directory, batch_size=32):
     list_ds = tf.data.Dataset.list_files(os.path.join(directory, '*.jpg'))
     image_ds = list_ds.map(lambda x: tf.io.read_file(x))
     image_ds = image_ds.map(decode_image)
-    images = list(image_ds)
-    return images
 
-images = read_images_tensorflow('path/to/images')
+    # Batch and prefetch data
+    image_ds = image_ds.batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    return image_ds
+
+images = read_images_tensorflow('path/to/images', 32)
