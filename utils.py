8aacbc31a36f2fcd6c0bad9979220ec465d59ce8
Sherzod <sfreedom9393@gmail.com>
2024-08-12 12:15:00 +0500

    utils.py is added

---
+++ utils.py
@@ -0,0 +1,21 @@
+import functools
+
+
+def time_decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        num_of_imgs = func(*args, **kwargs)
+        print(f"{kwargs['method']}: {time.time() - start_time}")
+        return num_of_imgs
+    return wrapper
+
+
+def time_decorator_async(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        start_time = time.time()
+        num_of_imgs = await func(*args, **kwargs)
+        print(f"{kwargs.get('method', 'async')}: {time.time() - start_time}")
+        return num_of_imgs
+    return wrapper
