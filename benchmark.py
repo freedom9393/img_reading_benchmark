4e6902487072b14b9d2fc858edb670264b62e711
Sherzod <sfreedom9393@gmail.com>
2024-08-12 12:38:37 +0500

    reading with asynchronously is added

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,11 @@
 import gc
+import asyncio
+
 from read_with_tensorflow import generate_with_tf
 from read_with_loop import read_images_iteratively
 from read_with_multithreading import read_images_multithreading
+from read_with_asynchronous import read_images_async
+
 
 source_dir = "/home/real/example_imgs/example"
 target_size = (640, 640)
@@ -11,5 +15,6 @@ threads = 4
 generate_with_tf(source_dir, batch_size, target_size)
 read_images_iteratively(source_dir, method='loop')
 read_images_multithreading(source_dir, method='multithread', max_workers=threads)
+asyncio.run(read_images_async(source_dir, method='async'))
 
 gc.collect()
--- read_with_asynchronous.py
+++ read_with_asynchronous.py
@@ -1,25 +1,37 @@
 import os
+import gc
 import cv2
 import asyncio
 import aiofiles
+import numpy as np
 
-async def read_image_async(filename):
-    async with aiofiles.open(filename, mode='rb') as f:
-        content = await f.read()
-        img_array = np.asarray(bytearray(content), dtype=np.uint8)
-        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        return img
+from utils import time_decorator_async
 
-async def read_images_async(directory):
+
+async def read_image_async(filename, semaphore):
+    async with semaphore:
+        async with aiofiles.open(filename, mode='rb') as f:
+            content = await f.read()
+            img_array = np.asarray(bytearray(content), dtype=np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            return img
+
+
+@time_decorator_async
+async def read_images_async(directory, **kwargs):
     images = []
     tasks = []
+    semaphore = asyncio.Semaphore(10)  # Start with a limit of 10 concurrent tasks
+    # print(f"Reading images from directory: {directory}")
     for filename in os.listdir(directory):
         if filename.endswith(".jpg") or filename.endswith(".png"):
-            tasks.append(read_image_async(os.path.join(directory, filename)))
+            filepath = os.path.join(directory, filename)
+            tasks.append(read_image_async(filepath, semaphore))
+            # print(f"Scheduled task for {filepath}")
     results = await asyncio.gather(*tasks)
     for img in results:
         if img is not None:
             images.append(img)
-    return images
 
-images = asyncio.run(read_images_async('path/to/images'))
+    gc.collect()
+    return len(images)
